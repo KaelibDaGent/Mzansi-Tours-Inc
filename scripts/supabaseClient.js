49eacf5a50e7511c7ca071b349ce52f8e64c4a52
Katleho Khosi <164510292+KaelibDaGent@users.noreply.github.com>
2026-07-31 19:41:08 +0200

    Add files via upload

---
+++ scripts/supabaseClient.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://jotbhavmyqqedhjcudwo.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_B1rJj0WN5I1vDEi6MoFyMw_-VFxZ09V";
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
